f13886c35e1e59d3ccdfe2eef023502efcdad691
JadejaShu <139604204+JadejaShu@users.noreply.github.com>
2024-01-07 02:52:15 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -14,62 +14,65 @@ TEMPLATE_PATH_AUDIO = 'Adarsh/template/req.html'
 TEMPLATE_PATH_OTHER = 'Adarsh/template/dl.html'
 
 async def render_page(id, secure_hash, quality='low'):
-    file_data = await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
-    if file_data.unique_id[:6] != secure_hash:
-        logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
-        logging.debug(f"Invalid hash for message with - ID {id}")
-        raise InvalidHash
+    try:
+        file_data = await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
+        if file_data.unique_id[:6] != secure_hash:
+            logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
+            logging.debug(f"Invalid hash for message with - ID {id}")
+            raise InvalidHash
 
-    filename = file_data.file_name
-    src = urllib.parse.urljoin(Var.URL, f'{secure_hash}{str(id)}')
-    template_path = (
-        TEMPLATE_PATH_VIDEO if file_data.mime_type.startswith('video') else
-        TEMPLATE_PATH_AUDIO if file_data.mime_type.startswith('audio') else
-        TEMPLATE_PATH_OTHER
-    )
-
-    async with aiofiles.open(template_path) as r:
-        tag = file_data.mime_type.split('/')[0].strip()
-        heading = (
-            f'Watch {filename}' if tag == 'video' else
-            f'Listen {filename}' if tag == 'audio' else
-            f'Download {filename}'
+        filename = file_data.file_name
+        src = urllib.parse.urljoin(Var.URL, f'{secure_hash}{str(id)}')
+        template_path = (
+            TEMPLATE_PATH_VIDEO if file_data.mime_type.startswith('video') else
+            TEMPLATE_PATH_AUDIO if file_data.mime_type.startswith('audio') else
+            TEMPLATE_PATH_OTHER
         )
-        html = (await r.read()).replace('tag', tag).format(title=heading, filename=filename, src=src, quality=quality)
-        logging.info(f"1st HTML {html}")
 
-    if tag == 'other':
-        async with aiohttp.ClientSession() as s:
-            async with s.get(src) as u:
-                heading = f'Download {filename}'
-                file_size = humanbytes(int(u.headers.get('Content-Length')))
-                async with aiofiles.open(TEMPLATE_PATH_OTHER) as r:
-                    html = (await r.read()).format(title=heading, filename=filename, src=src, quality=quality)
-                    logging.info(f"2nd HTML {html}")
-                    
-    current_url = f'{Var.URL}/{str(id)}/{filename}?hash={secure_hash}&quality={quality}'
-    logging.info(f"{current_url}")
-    html_code = f'''
-    <p>
-        <center><h5>Click on 👇 button to watch/download in your favorite player</h5></center>
+        async with aiofiles.open(template_path) as r:
+            tag = file_data.mime_type.split('/')[0].strip()
+            heading = (
+                f'Watch {filename}' if tag == 'video' else
+                f'Listen {filename}' if tag == 'audio' else
+                f'Download {filename}'
+            )
+            html = (await r.read()).replace('tag', tag).format(title=heading, filename=filename, src=src, quality=quality)
+            logging.info(f"1st HTML: {html}")
+
+        if tag == 'other':
+            async with aiohttp.ClientSession() as s:
+                async with s.get(src) as u:
+                    heading = f'Download {filename}'
+                    file_size = humanbytes(int(u.headers.get('Content-Length')))
+                    async with aiofiles.open(TEMPLATE_PATH_OTHER) as r:
+                        html = (await r.read()).format(title=heading, filename=filename, src=src, quality=quality)
+                        logging.info(f"2nd HTML: {html}")
+
+        current_url = f'{Var.URL}/{str(id)}/{filename}?hash={secure_hash}&quality={quality}'
+        logging.info(f"Current URL: {current_url}")
+        html_code = f'''
+        <p>
+            <center><h5>Click on 👇 button to watch/download in your favorite player</h5></center>
+            <center>
+                <button onclick="window.location.href = '{current_url}'">Save in your gallery</button> &nbsp
+            </center>
+        </p>
+        </p>
         <center>
-            <button onclick="window.location.href = '{current_url}'">Save in your gallery</button> &nbsp
+            <h2>
+                <a href="https://telegram.me/Mr_harsh_008">
+                    <img src="https://graph.org/file/b57cdba982191a25db535.jpg" alt="BotszList" width="150" height="75">
+                </a>
+            </h2>
         </center>
-    </p>
-    </p>
-    <center>
-        <h2>
-            <a href="https://telegram.me/Mr_harsh_008">
-                <img src="https://graph.org/file/b57cdba982191a25db535.jpg" alt="BotszList" width="150" height="75">
-            </a>
-        </h2>
-    </center>
-    '''
+        '''
 
-    html += html_code
-    return html
+        html += html_code
+        return html
 
-
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        raise
 
 # from Adarsh.vars import Var
 # from Adarsh.bot import StreamBot
@@ -93,6 +96,7 @@ async def render_page(id, secure_hash, quality='low'):
 #         logging.debug(f"Invalid hash for message with - ID {id}")
 #         raise InvalidHash
 
+#     filename = file_data.file_name
 #     src = urllib.parse.urljoin(Var.URL, f'{secure_hash}{str(id)}')
 #     template_path = (
 #         TEMPLATE_PATH_VIDEO if file_data.mime_type.startswith('video') else
@@ -103,23 +107,23 @@ async def render_page(id, secure_hash, quality='low'):
 #     async with aiofiles.open(template_path) as r:
 #         tag = file_data.mime_type.split('/')[0].strip()
 #         heading = (
-#             f'Watch {file_data.file_name}' if tag == 'video' else
-#             f'Listen {file_data.file_name}' if tag == 'audio' else
-#             f'Download {file_data.file_name}'
+#             f'Watch {filename}' if tag == 'video' else
+#             f'Listen {filename}' if tag == 'audio' else
+#             f'Download {filename}'
 #         )
-#         html = (await r.read()).replace('tag', tag).format(heading, file_data.file_name, src, quality)
+#         html = (await r.read()).replace('tag', tag).format(title=heading, filename=filename, src=src, quality=quality)
 #         logging.info(f"1st HTML {html}")
 
 #     if tag == 'other':
 #         async with aiohttp.ClientSession() as s:
 #             async with s.get(src) as u:
-#                 heading = f'Download {file_data.file_name}'
+#                 heading = f'Download {filename}'
 #                 file_size = humanbytes(int(u.headers.get('Content-Length')))
 #                 async with aiofiles.open(TEMPLATE_PATH_OTHER) as r:
-#                     html = (await r.read()).format(heading, file_data.file_name, src, quality)
+#                     html = (await r.read()).format(title=heading, filename=filename, src=src, quality=quality)
 #                     logging.info(f"2nd HTML {html}")
                     
-#     current_url = f'{Var.URL}/{str(id)}/{file_data.file_name}?hash={secure_hash}&quality={quality}'
+#     current_url = f'{Var.URL}/{str(id)}/{filename}?hash={secure_hash}&quality={quality}'
 #     logging.info(f"{current_url}")
 #     html_code = f'''
 #     <p>
@@ -137,5 +141,6 @@ async def render_page(id, secure_hash, quality='low'):
 #         </h2>
 #     </center>
 #     '''
+
 #     html += html_code
 #     return html
