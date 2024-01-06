d0229c40b06ae7f3f0e10c3811769d128f8aac53
JadejaShu <139604204+JadejaShu@users.noreply.github.com>
2024-01-07 00:03:38 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -1,16 +1,4 @@
-from Adarsh.vars import Var
-from Adarsh.bot import StreamBot
-from Adarsh.utils.human_readable import humanbytes
-from Adarsh.utils.file_properties import get_file_ids
-from Adarsh.server.exceptions import InvalidHash
-import urllib.parse
-import aiofiles
-import logging
-import aiohttp
-
-TEMPLATE_PATH_VIDEO = 'Adarsh/template/req.html'
-TEMPLATE_PATH_AUDIO = 'Adarsh/template/req.html'
-TEMPLATE_PATH_OTHER = 'Adarsh/template/dl.html'
+# ... (existing imports and code)
 
 async def render_page(id, secure_hash, quality='low'):
     file_data = await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
@@ -33,7 +21,7 @@ async def render_page(id, secure_hash, quality='low'):
             f'Listen {file_data.file_name}' if tag == 'audio' else
             f'Download {file_data.file_name}'
         )
-        html = (await r.read()).replace('tag', tag) % (file_data.file_name, file_data.file_name, src, quality)
+        html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src, quality)
 
     if tag == 'other':
         async with aiohttp.ClientSession() as s:
@@ -41,43 +29,30 @@ async def render_page(id, secure_hash, quality='low'):
                 heading = f'Download {file_data.file_name}'
                 file_size = humanbytes(int(u.headers.get('Content-Length')))
                 async with aiofiles.open(TEMPLATE_PATH_OTHER) as r:
-                    html = (await r.read()) % (heading, file_data.file_name, src, file_size)
+                    html = (await r.read()) % (heading, file_data.file_name, src, file_size, quality)
 
     current_url = f'{Var.URL}/{str(id)}/{file_data.file_name}?hash={secure_hash}&quality={quality}'
-    html_code = (
-    '<p>\n'
-    '    <center><h5>Click on 👇 button to watch/download in your favorite player</h5></center>\n'
-    '    <center>\n'
-    '        <button class="cybr-btn player" style="font-size: 20px; background-color: skyblue; border-radius: 10px;" '
-    'data-plyr-provider="html" data-plyr-embed-id="{}">Play Video</button> &nbsp\n'
-    '    </center>\n'
-    '</p>\n'
-    '<p>\n'
-    '    <center>\n'
-    '        <!-- Add quality selection dropdown -->\n'
-    '        <select id="quality-selector">\n'
-    '            <option value="low">Low Quality</option>\n'
-    '            <option value="medium">Medium Quality</option>\n'
-    '            <option value="high">High Quality</option>\n'
-    '        </select>\n'
-    '    </center>\n'
-    '</p>\n'
-    '<script src="https://cdn.plyr.io/3.6.12/plyr.js"></script>\n'
-    '<script>\n'
-    '    document.addEventListener(\'DOMContentLoaded\', () => {{\n'
-    '        const players = Plyr.setup(\'.player\', {{\n'
-    '            controls: [\'play-large\', \'play\', \'progress\', \'current-time\', \'mute\', \'volume\', \'fullscreen\'],\n'
-    '        }});\n'
-    '\n'
-    '        const qualitySelector = document.getElementById(\'quality-selector\');\n'
-    '        qualitySelector.addEventListener(\'change\', function() {{\n'
-    '            const selectedQuality = this.value;\n'
-    '            const downloadButton = document.querySelector(\'.cybr-btn\');\n'
-    '            const newDownloadURL = downloadButton.href.replace(\'{{quality}}\', selectedQuality);\n'
-    '            downloadButton.href = newDownloadURL;\n'
-    '        }});\n'
-    '        qualitySelector.value = \'{quality}\';\n'
-    '    }});\n'
-    '</script>'
-    )
-    formatted_html_code = html_code.format(current_url=current_url, quality=quality)
+    html_code = f'''
+    <p>
+        <center><h5>Click on 👇 button to watch/download in your favorite player</h5></center>
+        <center>
+            <button style="font-size: 20px; background-color: skyblue; border-radius: 10px;" onclick="window.location.href = 'intent:{current_url}#Intent;package=com.mxtech.videoplayer.ad;S.title={file_data.file_name};end'">MX player</button> &nbsp
+            <button style="font-size: 20px; background-color: orange; border-radius: 10px;" onclick="window.location.href = 'vlc://{current_url}'">VLC player</button> &nbsp <br>
+            <p>&nbsp</p>
+            <button style="font-size: 20px; background-color: red; border-radius: 10px;" onclick="window.location.href = 'playit://playerv2/video?url={current_url}&amp;title={file_data.file_name}'">Playit player</button> &nbsp <br>
+            <p>&nbsp</p>
+            <button style="font-size: 20px; background-color: yellow; border-radius: 10px;" onclick="window.location.href = '{current_url}'">Save in your gallery</button> &nbsp
+        </center>
+    </p>
+    </p>
+    <center>
+        <h2>
+            <a href="https://telegram.me/Mr_harsh_008">
+                <img src="https://graph.org/file/b57cdba982191a25db535.jpg" alt="BotszList" width="150" height="75">
+            </a>
+        </h2>
+    </center>
+    '''
+
+    html += html_code
+    return html
